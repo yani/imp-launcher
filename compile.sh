4003bd06b1a9e447145e59cecadd04bb465d5421
Yani <yani@protonmail.com>
2023-08-02 04:05:17 +0200

    improve compile script

--- compile.sh
+++ compile.sh
@@ -8,3 +8,9 @@ rm -f ./build/META-INF/MANIFEST.MF
 javac -d ./build -cp lib/jarchivelib-0.7.1-jar-with-dependencies.jar:lib/json-simple-1.1.1.jar:lib/pecoff4j-0.4.0.jar -source 1.8 -target 1.8 *.java ./Settings/*.java
 jar cvfm ./implauncher.jar manifest.txt -C ./build . -C . ./implauncher-data
 rm -f -R ./build
+chmod +x ./implauncher.jar
+
+echo "-----------------------------------------------------------"
+echo "Output: ./implauncher.jar"
+echo ""
+echo "JAR created!"
