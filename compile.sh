34c8443a1992a5b465e9e6bc3bdea7028527d753
Yani <yani@protonmail.com>
2023-08-02 03:54:34 +0200

    remove old jar file during build

--- compile.sh
+++ compile.sh
@@ -1,6 +1,4 @@
-#javac -d ./build -cp lib/jarchivelib-0.7.1-jar-with-dependencies.jar:lib/json-simple-1.1.1.jar:lib/pecoff4j-0.4.0.jar -source 1.8 -target 1.8 *.java ./Settings/*.java
-#jar cvfm ./implauncher.jar manifest.txt -C ./build . -C . ./implauncher-data -C ./lib .
-
+rm -f implauncher.jar
 rm -f -R ./build
 mkdir ./build
 unzip -o ./lib/jarchivelib-0.7.1-jar-with-dependencies.jar -d ./build
