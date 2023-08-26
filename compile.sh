b5af540d05061cf0cbd327cfd0db6a2a6888e033
Yani <yani@protonmail.com>
2023-08-26 14:48:10 +0200

    remove debug information in compilation

--- compile.sh
+++ compile.sh
@@ -12,7 +12,7 @@ mkdir ./build/self-updater
 mkdir ./build-updater
 echo "-----------------------------------------------------------"
 echo "[+] Compiling updater..."
-javac -d ./build-updater -source 1.8 -target 1.8 ./updater/*.java
+javac -d ./build-updater -g:none -source 1.8 -target 1.8 ./updater/*.java
 echo "-----------------------------------------------------------"
 echo "[+] Creating .jar from updater .class files..."
 jar cvfe ./build/self-updater/implauncher-updater.jar updater.Main -C ./build-updater .
@@ -25,7 +25,7 @@ unzip -o ./lib/semver4j-5.1.0.jar -d ./build
 rm -f ./build/META-INF/MANIFEST.MF
 echo "-----------------------------------------------------------"
 echo "[+] Compiling application..."
-javac -d ./build -cp lib/jarchivelib-0.7.1-jar-with-dependencies.jar:lib/json-simple-1.1.1.jar:lib/pecoff4j-0.4.0.jar:lib/semver4j-5.1.0.jar -source 1.8 -target 1.8 ./src/*.java ./src/Setting/*.java
+javac -d ./build -cp lib/jarchivelib-0.7.1-jar-with-dependencies.jar:lib/json-simple-1.1.1.jar:lib/pecoff4j-0.4.0.jar:lib/semver4j-5.1.0.jar -g:none -source 1.8 -target 1.8 ./src/*.java ./src/Setting/*.java
 echo "-----------------------------------------------------------"
 echo "[+] Creating .jar from application .class files..."
 jar cvfm ./output/implauncher.jar manifest.txt -C ./build . -C . ./implauncher-data
