0532e89d3ebc2589113032b00734b61b149c3941
Yani <yani@protonmail.com>
2023-08-26 12:34:51 +0200

    Revert "Revert "add semver4j library""
    This reverts commit da393706669a2196c50e18c4ad1c4f17548c306c.

--- compile.sh
+++ compile.sh
@@ -21,10 +21,11 @@ echo "[+] Copying library code into main build directory..."
 unzip -o ./lib/jarchivelib-0.7.1-jar-with-dependencies.jar -d ./build
 unzip -o ./lib/json-simple-1.1.1.jar -d ./build
 unzip -o ./lib/pecoff4j-0.4.0.jar -d ./build
+unzip -o ./lib/semver4j-3.1.0.jar -d ./build
 rm -f ./build/META-INF/MANIFEST.MF
 echo "-----------------------------------------------------------"
 echo "[+] Compiling application..."
-javac -d ./build -cp lib/jarchivelib-0.7.1-jar-with-dependencies.jar:lib/json-simple-1.1.1.jar:lib/pecoff4j-0.4.0.jar -source 1.8 -target 1.8 ./src/*.java ./src/Setting/*.java
+javac -d ./build -cp lib/jarchivelib-0.7.1-jar-with-dependencies.jar:lib/json-simple-1.1.1.jar:lib/pecoff4j-0.4.0.jar:lib/semver4j-3.1.0.jar -source 1.8 -target 1.8 ./src/*.java ./src/Setting/*.java
 echo "-----------------------------------------------------------"
 echo "[+] Creating .jar from application .class files..."
 jar cvfm ./output/implauncher.jar manifest.txt -C ./build . -C . ./implauncher-data
