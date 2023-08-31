d8351a12ff2375faa3eee2b13576ee8276a3f32d
Yani <yani@protonmail.com>
2023-08-31 10:37:48 +0200

    add prototype to game build dropdown in settings window

--- src/Setting/GameBuild.java
+++ src/Setting/GameBuild.java
@@ -3,6 +3,7 @@ package src.Setting;
 import javax.swing.JComboBox;
 
 import src.KfxReleaseType;
+import src.Main;
 
 public class GameBuild extends SettingsComboBox {
 
@@ -11,7 +12,12 @@ public class GameBuild extends SettingsComboBox {
 
         settingsComboBox.put(KfxReleaseType.STABLE.toString(), "Stable");
         settingsComboBox.put(KfxReleaseType.ALPHA.toString(), "Alpha");
-        // settingsComboBox.put(KfxReleaseType.PROTOTYPE.toString(), "Prototype");
+
+        // Add Prototype to text box if current game build is a prototype.
+        // This makes it so it's only visible if we are on a prototype.
+        if (Main.kfxReleaseType == KfxReleaseType.PROTOTYPE) {
+            settingsComboBox.put(KfxReleaseType.PROTOTYPE.toString(), "Prototype");
+        }
 
         return settingsComboBox.createComponent(currentKey);
     }
