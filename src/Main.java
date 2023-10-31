d8adfbea70111880d2913368c0f97e04f88d4357
Yani <yani@protonmail.com>
2023-10-31 08:18:47 +0100

    bump version to 0.6.0

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ public class Main extends JFrame {
 
     public static Main main;
 
-    public static String impLauncherVersion = "0.5.0";
+    public static String impLauncherVersion = "0.6.0";
 
     public static String launcherRootDir;
 
