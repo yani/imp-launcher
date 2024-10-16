da66dca856c8b8ebbd8a1d71e1f1e50243467ee6
Yani <yani@protonmail.com>
2024-10-16 12:51:07 +0200

    add unpushed version update

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ public class Main extends JFrame {
 
     public static Main main;
 
-    public static String impLauncherVersion = "0.10.0";
+    public static String impLauncherVersion = "0.10.1";
 
     public static String launcherRootDir;
 
